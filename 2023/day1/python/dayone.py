4108a8dfaba928eacfa816ecdb766c4096674965
Damiano Lampisti <35961105+damianolampisti00@users.noreply.github.com>
2023-12-04 10:07:03 +0100

    D1 part two not finished

--- 2023/day1/python/dayone.py
+++ 2023/day1/python/dayone.py
@@ -1,14 +1,35 @@
 def retrievedigits(thislist):
   digits = ""
   enum=["one","two","three","four","five","six","seven","eight","nine"]
-  #find the digits in the line
+  number=[]
+  positions={}
+  
   for x in thislist:
     if x.isdigit():
-      digits+=x
-  number=[digits[0],digits[(len(digits))-1]]
+      for y in range(thislist.count(x)):
+        positions[[i for i, z in enumerate(thislist) if z == x][y]] = x  
+  
+  for x in enum:
+    for y in range(thislist.count(x)):
+      if x in thislist:
+        print([i for i, z in enumerate(thislist) if z == x][y])
+
+  sorted_dict = dict(sorted(positions.items()))
+
+  if (list(sorted_dict.values())[0]).isdigit():
+    number.append(list(sorted_dict.values())[0])
+  else:
+    number.append((enum.index(list(sorted_dict.values())[0]))+1)
+  
+  if (list(sorted_dict.values())[len(sorted_dict.values())-1]).isdigit():
+    number.append(list(sorted_dict.values())[len(sorted_dict.values())-1])
+  else:
+    number.append((enum.index(list(sorted_dict.values())[len(sorted_dict.values())-1]))+1)
+  #create and return an array with the first and the last digit of a line
+
   return number
 
-f = open("list.txt", "r")
+f = open("C:/Users/tomir/Documents/GitHub/advent-of-code/2023/day1/python/values.txt", "r")
 sumof=0
 #for every line, call the function and sum the result
 for x in f:
@@ -16,3 +37,5 @@ for x in f:
   sumof+=num
 
 print(sumof)
+
+
